01efd7c71a3b186f090c5f14c23e40f32dcca4b8
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-09-25 01:09:19 +0900

    Lチカ

--- main.cpp
+++ main.cpp
@@ -16,6 +16,12 @@ int main() {
   // Init GPIO
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  // Blink LED
   while (1) {
+    gpio_put(LED_PIN, 1);
+    sleep_ms(250);
+    gpio_put(LED_PIN, 0);
+    sleep_ms(250);
   }
 }
