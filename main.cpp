1e7fc2ade82fc25ed40a3b315d7c9d8d0fbd7fb1
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-09-25 01:09:02 +0900

    comment

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@
 const uint LED_PIN = 25;
 
 int main() {
+  // Project discripsion.
   bi_decl(bi_program_description("This is a jade firmware test for RP2040."));
   bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+
+  // Init stdio
   stdio_init_all();
+
+  // Init GPIO
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
   while (1) {
