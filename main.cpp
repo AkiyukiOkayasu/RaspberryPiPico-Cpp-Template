04cc43c00b8cb0a62a27b29a456419be9cc54544
Akiyuki Okayasu <akiyuki@tkrworks.net>
2021-09-25 00:52:49 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+const uint LED_PIN = 25;
+
+int main() {
+  bi_decl(bi_program_description("This is a jade firmware test for RP2040."));
+  bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+  stdio_init_all();
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
+  while (1) {
+  }
+}
