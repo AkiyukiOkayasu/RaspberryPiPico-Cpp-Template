350357b667d2aaf4e354457860be5bb3cffbc65b
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-09-26 14:53:52 +0900

    ピン定数を別ヘッダーに追加

---
+++ constants.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "pico/stdlib.h"
+#include <cstdint>
+
+namespace pin {
+inline constexpr unsigned int led = PICO_DEFAULT_LED_PIN; // 25
+}
--- main.cpp
+++ main.cpp
@@ -1,29 +1,28 @@
+#include "constants.hpp"
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 #include "pico/stdlib.h"
 #include <cstdint>
 #include <cstdio>
 
-constexpr unsigned int LED_PIN = PICO_DEFAULT_LED_PIN; // 25
-
 int main() {
   // Project discripsion.
   bi_decl(bi_program_description("This is a skelton project for RP2040."));
-  bi_decl(bi_1pin_with_name(LED_PIN, "Pico on-board LED"));
+  bi_decl(bi_1pin_with_name(pin::led, "Pico on-board LED"));
 
   // Init stdio
   stdio_init_all();
 
   // Init GPIO
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
+  gpio_init(pin::led);
+  gpio_set_dir(pin::led, GPIO_OUT);
 
   // Blink LED
   while (1) {
     std::printf("test\n");
-    gpio_put(LED_PIN, 1);
+    gpio_put(pin::led, 1);
     sleep_ms(250);
-    gpio_put(LED_PIN, 0);
+    gpio_put(pin::led, 0);
     sleep_ms(250);
   }
 }
