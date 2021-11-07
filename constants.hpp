fce383e696d1aef7976ae28cfd0ffdb18b97c1a4
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-11-07 21:29:25 +0900

    style: clang-format

--- constants.hpp
+++ constants.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include "pico/stdlib.h"
+
 #include <cstdint>
 
-namespace pin {
+namespace pin
+{
 inline constexpr unsigned int led = PICO_DEFAULT_LED_PIN; // 25
 }
--- main.cpp
+++ main.cpp
@@ -2,27 +2,30 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 #include "pico/stdlib.h"
+
 #include <cstdint>
 #include <cstdio>
 
-int main() {
-  // Project discripsion.
-  bi_decl(bi_program_description("This is a skelton project for RP2040."));
-  bi_decl(bi_1pin_with_name(pin::led, "Pico on-board LED"));
+int main()
+{
+    // Project discripsion.
+    bi_decl (bi_program_description ("This is a skelton project for RP2040."));
+    bi_decl (bi_1pin_with_name (pin::led, "Pico on-board LED"));
 
-  // Init stdio
-  stdio_init_all();
+    // Init stdio
+    stdio_init_all();
 
-  // Init GPIO
-  gpio_init(pin::led);
-  gpio_set_dir(pin::led, GPIO_OUT);
+    // Init GPIO
+    gpio_init (pin::led);
+    gpio_set_dir (pin::led, GPIO_OUT);
 
-  // Blink LED
-  while (1) {
-    std::printf("test\n");
-    gpio_put(pin::led, 1);
-    sleep_ms(250);
-    gpio_put(pin::led, 0);
-    sleep_ms(250);
-  }
+    // Blink LED
+    while (1)
+    {
+        std::printf ("test\n");
+        gpio_put (pin::led, 1);
+        sleep_ms (250);
+        gpio_put (pin::led, 0);
+        sleep_ms (250);
+    }
 }
