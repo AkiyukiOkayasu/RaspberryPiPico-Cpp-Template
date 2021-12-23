4c0bc8f11a82d02da1a6ff139198091af07e5fd2
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-12-23 22:00:22 +0900

    Add DCDC ripple remove function

--- main.cpp
+++ main.cpp
@@ -12,11 +12,21 @@
 #include <cstdint>
 #include <cstdio>
 
+///DCDC PSM control for reduce 3.3V output ripple.
+void reduceDcdcRipple()
+{
+    gpio_init (pin::DCDC::PS_CONTROL);
+    gpio_set_dir (pin::DCDC::PS_CONTROL, GPIO_OUT);
+    gpio_put (pin::DCDC::PS_CONTROL, DCDC::PWM); // PWM mode for reduce ripple.
+}
+
 int main()
 {
     // Project discripsion.
     bi_decl (bi_program_description ("This is a skelton project for RP2040."));
     bi_decl (bi_1pin_with_name (pin::led, "Pico on-board LED"));
+
+    reduceDcdcRipple();
 
     // Init stdio
     stdio_init_all();
@@ -27,6 +37,8 @@ int main()
 
     while (1)
     {
+        busy_wait_ms (1);
+
         std::printf ("test\n");                                     //USB-Serial console output
         SEGGER_RTT_WriteString (0, "Hello World from SEGGER!\r\n"); //SEGGER RTT console output
 
